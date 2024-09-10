f86a092487b34a17548e8d1c56b8d4b89bdc0c94
adithyamode <123758039+adithyamode@users.noreply.github.com>
2024-09-10 10:28:46 +0530

    Update Navbar.js

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -6,22 +6,42 @@ const NavbarComponent = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
       <Container>
-        <Navbar.Brand as={NavLink} to="/">
+        <Navbar.Brand as={NavLink} to="/" className="text-warning">
           EZPay
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/" className="nav-link">
+            <Nav.Link
+              as={NavLink}
+              to="/"
+              className="nav-link text-light"
+              activeClassName="text-warning"
+            >
               Home
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/payment" className="nav-link">
+            <Nav.Link
+              as={NavLink}
+              to="/payment"
+              className="nav-link text-light"
+              activeClassName="text-warning"
+            >
               Payment
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/payment-history" className="nav-link">
+            <Nav.Link
+              as={NavLink}
+              to="/payment-history"
+              className="nav-link text-light"
+              activeClassName="text-warning"
+            >
               Payment History
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/check-balance" className="nav-link">
+            <Nav.Link
+              as={NavLink}
+              to="/check-balance"
+              className="nav-link text-light"
+              activeClassName="text-warning"
+            >
               Check Balance
             </Nav.Link>
           </Nav>
